683dec70e5ef16d01fe145f83af3890b175596ea
deckerayers <eclipse12895@gmail.com>
2023-02-23 15:25:33 -0500

    Added Freame

--- shale.lofi/shale.lofi/MainPage.xaml.cs
+++ shale.lofi/shale.lofi/MainPage.xaml.cs
@@ -7,7 +7,8 @@ public partial class MainPage : ContentPage
 
 	public MainPage()
 	{
-
+		Shell.SetTabBarBackgroundColor(this, Color.FromArgb("666666"));
+		Shell.SetNavBarIsVisible(this, false);
         InitializeComponent();
 	}
 
